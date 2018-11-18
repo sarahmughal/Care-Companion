9dd8840a3183a2e6f50df0026c80e8fd26457758
jennifernj <jenniferneda@gmail.com>
2018-11-17 22:04:53 -0800

    Update content.js

--- content.js
+++ content.js
@@ -2,9 +2,9 @@ elem = () => {return document.getElementsByClassName("gLFyf")[0].value}
 document.getElementById('tsf').addEventListener("submit", function(e) {
         const mVal = elem();
         console.log(mVal);
-        if (mVal.includes('death')) {
-        	alert('everything ok?');
-        } else if (mval.includes('self-harm')) {
-        	alert('hello!');
-        }
-});
+        if (mVal.includes('calories in')) {
+        	alert("Hello! We're concerned that you may be focused on calorie counts, which can be a symptom of an eating disorder. We care about you, so please reach out to the eating disorder hotline if you need help: (800) 931-2237");
+        } else if (mVal.includes('kill myself')) {
+        	alert("Hi there! We wanted to make sure you're doing OK. If you'd like to talk to someone, the suicide prevention hotline is always available: 1-800-273-8255");
+        };
+    });
